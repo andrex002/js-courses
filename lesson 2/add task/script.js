d59e65043de306ee61aa36b72a3210590666f348
Andrey <sergeev-002@yandex.ru>
2018-03-29 20:58:48 +0300

    Урок 2. Дополнительное задание

---
+++ lesson 2/add task/script.js
@@ -0,0 +1,51 @@
+let x = 5; alert(x++);  
+//5
+
+//console.log([ ] + false - null + true);  
+//NaN
+
+//let y = 1; 
+//let x = y = 2; 
+//alert(x); 
+//2
+
+//console.log([ ] + 1 + 2);
+//12
+
+//alert( "1"[0] );
+//1
+
+//console.log(2 && 1 && null && 0 && undefined);
+//null
+
+//var a;
+//var b;
+//console.log(!!( a && b )); //false
+//console.log((a && b)); //undefined
+
+//alert( null || 2 && 3 || 4 );
+//3
+
+//a = [1, 2, 3];
+//b = [1, 2, 3];
+//if (a == b) {
+//console.log('Верно');
+//} else {
+//console.log('Неверно');
+//}
+//неверно
+
+//alert( +"Infinity" );
+//Infinity
+
+//a = "ёжик";
+//b = "яблоко";
+//if (a > b) {
+//	console.log('Верно');
+//} else {
+//	console.log('Неверно');
+//}
+//верно
+
+//console.log(0 || "" || 2 || undefined || true || falsе);
+//2
