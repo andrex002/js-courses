85077a8d8adda4a8d973193848f7a2442915a267
Andrey <sergeev-002@yandex.ru>
2018-04-08 11:58:02 +0300

    Урок 7. Задание 1

---
+++ lesson 7/js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', function () {
+	var tab = document.querySelectorAll('.info-header-tab');
+	var tabContent = document.querySelectorAll('.info-tabcontent');
+	var info = document.querySelector('.info-header');
+
+	function hideTabContent(a) {
+		for (var i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	}
+	hideTabContent(1);
+
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			hideTabContent(0);
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', function(event) {
+		var target = event.target;
+		if (target.className == 'info-header-tab') {
+			for (var i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	})
+});
