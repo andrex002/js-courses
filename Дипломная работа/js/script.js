66956bee86b68d022cabcc8f8e979f5cab2b77bd
Andrey <sergeev-002@yandex.ru>
2018-04-22 00:39:03 +0300

    Создание новой карточки кандидата

--- Дипломная работа/js/script.js
+++ Дипломная работа/js/script.js
@@ -15,4 +15,112 @@ window.addEventListener('DOMContentLoaded', function() {
 			customChild[i].style.display = 'block';
 		}
 	})
+
+	let mainCardsItem = document.querySelectorAll('.main-cards-item');
+	let name = mainCardsItem[1].querySelector('.name');
+	let age = mainCardsItem[1].querySelector('.age');
+	let sex = mainCardsItem[1].querySelector('.sex');
+	let views = mainCardsItem[1].querySelector('.views');
+	let bio = mainCardsItem[1].querySelector('.bio');
+	let personEasy = document.querySelector('.person-easy');
+
+	
+	let nameValue = document.getElementById('name');
+	let ageValue = document.getElementById('age');
+	let maleValue = document.getElementById('male');
+	let femaleValue = document.getElementById('female');
+	let viewsValue = document.getElementById('select');
+	let bioValue = document.getElementById('bio');
+	let radio = document.querySelector('.radio');
+	let inputSex = radio.getElementsByTagName('input');
+
+	nameValue.addEventListener('change', function() {
+		
+		if (isNaN(nameValue.value) && nameValue.value != '') {
+			name.textContent = nameValue.value;
+		}
+
+	});
+
+	ageValue.addEventListener('change', function() {
+		
+		if (isNaN(ageValue.value) || ageValue.value == '') {
+			age.textContent = '0 лет';
+		} else {
+			age.textContent = ageValue.value + ' лет';
+		}
+
+	});
+
+	radio.addEventListener('click', function(event) {
+		if (event.target && event.target.tagName == 'INPUT') {
+			sex.textContent = event.target.value;
+		}
+
+		if (event.target.value == 'Женский') {
+			personEasy.style.backgroundImage = 'url(img/construct-1.png)';
+			preview.style.backgroundImage = 'url(img/construct-1.png)';
+			photo3.style.backgroundImage = personEasy.style.backgroundImage;
+		} else {
+			personEasy.style.backgroundImage = 'url(img/construct-5.png)';
+			preview.style.backgroundImage = 'url(img/construct-5.png)';
+			photo3.style.backgroundImage = personEasy.style.backgroundImage;
+		}
+	});
+	
+	viewsValue.addEventListener('change', function() {
+		views.innerHTML = this.options[this.selectedIndex].value;
+	});
+
+	bioValue.addEventListener('change', function() {
+		bio.innerHTML = this.value;
+	});
+
+	//Слайдер
+	let prev = document.querySelector('.prev');
+	let next = document.querySelector('.next');
+	let preview = document.querySelector('.preview');
+	let photo3 = document.querySelector('.photo-3');
+	let slideIndex = 5;
+
+	function showSlides (n) {
+		if (n > 8 && male.checked) {
+			slideIndex = 5;
+		};
+		if (n < 5 && male.checked) {
+			slideIndex = 8;
+		}
+		if (n > 4 && female.checked) {
+			slideIndex = 1;
+		}
+		if (n < 1 && female.checked) {
+			slideIndex = 4;
+		}
+	}
+	
+	function plusSlides (n) {
+		
+		showSlides(slideIndex += n)
+		preview.style.backgroundImage = 'url(img/construct-' + slideIndex +'.png)';
+		personEasy.style.backgroundImage = 'url(img/construct-' + slideIndex +'.png)';
+		photo3.style.backgroundImage = personEasy.style.backgroundImage;
+	}
+
+	
+	next.addEventListener('click', function() {
+		plusSlides(1);
+
+	});
+
+	prev.addEventListener('click', function() {
+		plusSlides(-1);
+	});
+	//Слайдер - конец
+
+	let ready = document.getElementById('ready');
+
+	ready.addEventListener('click', function() {
+		custom.style.display = 'none';
+		main.style.display = 'block';
+	});
 });
